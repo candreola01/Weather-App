4418c704b718490ecbc83cd0e62ff10c42d9596d
Christine Andreola <andreola.christine@gmail.com>
2022-08-16 10:02:52 -0500

    Add event listner

--- scripts.js
+++ scripts.js
@@ -1,2 +1,14 @@
 // OpenWeatherMap API. Do not share it publicly.
 const api = '48742a43ee247a11bbc2c81076054696';
+
+//event listner that runs every time the page is loaded
+window.addEventListener('load', ()=>{
+    let long; //longitude
+    let lat; //latitude
+    //Accessing geolocation of user
+    if (navigator.geolaction) {
+        navigator.geolocation.getCurrentPosition((position)={
+
+        });
+    }
+});
