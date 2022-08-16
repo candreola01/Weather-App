cf37288d849eb5197e340e3f84749d7b2d274e1e
Christine Andreola <andreola.christine@gmail.com>
2022-08-16 10:13:50 -0500

    Use fetch to get data

--- scripts.js
+++ scripts.js
@@ -11,7 +11,19 @@ window.addEventListener('load', ()=>{
             //storing longitude and latitude in variables
             long = position.coords.longitude;
             lat = position.coords.latitude;
-            const base = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${api}&units=metric`
+            const base = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${api}&units=metric`;
+
+            //using fetch to get data
+            fetch(base)
+            .then((response)=>{
+                return response.json();
+            })
+            .then((data)=>{
+                const {temp} = data.main;
+                const place = data.name;
+                const {description, icon} = data.weather[0];
+                const {sunrise, sunset} = data.sys;
+            });
         });
     }
 });
