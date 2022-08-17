7b2364eae1a663c0657cb123c8ed6d096d6a1619
Christine Andreola <andreola.christine@gmail.com>
2022-08-17 07:30:46 -0500

    Add icon, farenheight and GMT

--- scripts.js
+++ scripts.js
@@ -23,6 +23,14 @@ window.addEventListener('load', ()=>{
                 const place = data.name;
                 const {description, icon} = data.weather[0];
                 const {sunrise, sunset} = data.sys;
+
+                const iconUrl = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+
+                const fahrenheit = (temp * 9) / 5 + 32;
+
+                //converting epoch(unix) time to GMT
+                const sunriseGMT = new Date(sunrise * 1000);
+                const sunsetGMT = new Date(sunset * 1000);
             });
         });
     }
