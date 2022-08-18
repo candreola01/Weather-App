db25a1cc2fe2754dc7d830865f745d7deef647dc
Christine Andreola <andreola.christine@gmail.com>
2022-08-18 08:16:11 -0500

    Add comma

--- scripts.js
+++ scripts.js
@@ -5,5 +5,5 @@ let weather = {
             "https://api.openweathermap.org/data/2.5/weather?lat=87.6298&lon=41.8781&units=metric&appid=48742a43ee247a11bbc2c81076054696"
         ).then((response)=> response.json())
         .then((data)=> console.log(data));
-    };
-}
+    },
+};
