02c8d82efdddb9285bab89754d3be30dc8cc19a2
Christine Andreola <andreola.christine@gmail.com>
2022-08-18 08:21:08 -0500

    Fix apiKey with this.

--- scripts.js
+++ scripts.js
@@ -1,15 +1,18 @@
 let weather = {
     "apiKey": "48742a43ee247a11bbc2c81076054696",
-    fetchWeather: function () {
+    fetchWeather: function (lat, lon) {
         fetch(
             "https://api.openweathermap.org/data/2.5/weather?lat=" 
             + lat 
             + "&lon=" 
             + lon 
             + "&units=metric&appid=" 
-            + appKey,
+            + this.apiKey,
         ).then((response)=> response.json())
         .then((data)=> console.log(data));
     },
+    displayWeather: function(data) {
+
+    }
 };
  
