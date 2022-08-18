febfab6906f0a0ab2806dd51ed22db741bf4fe8c
Christine Andreola <andreola.christine@gmail.com>
2022-08-18 08:24:51 -0500

    Add display weather function

--- scripts.js
+++ scripts.js
@@ -12,7 +12,10 @@ let weather = {
         .then((data)=> console.log(data));
     },
     displayWeather: function(data) {
-
+        const {name} = data;
+        const {icon, description} = data.weather;
+        const {temp, humidity} = data.main;
+        const {speed} = data.wind;
     }
 };
  
