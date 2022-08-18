f77e8c31da22dbcdd6210ac59448cbac033909df
Christine Andreola <andreola.christine@gmail.com>
2022-08-18 08:19:18 -0500

    Add parameter

--- scripts.js
+++ scripts.js
@@ -2,8 +2,14 @@ let weather = {
     "apiKey": "48742a43ee247a11bbc2c81076054696",
     fetchWeather: function () {
         fetch(
-            "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&units=metric&appid=48742a43ee247a11bbc2c81076054696"
+            "https://api.openweathermap.org/data/2.5/weather?lat=" 
+            + lat 
+            + "&lon=" 
+            + lon 
+            + "&units=metric&appid=" 
+            + appKey,
         ).then((response)=> response.json())
         .then((data)=> console.log(data));
     },
 };
+ 
