3dad1dac5dee555f2fb5038631c3fe98a3544f31
Christine Andreola <andreola.christine@gmail.com>
2022-08-18 10:34:08 -0500

    Add document.querySelectors

--- scripts.js
+++ scripts.js
@@ -1,21 +1,27 @@
 let weather = {
     "apiKey": "48742a43ee247a11bbc2c81076054696",
-    fetchWeather: function (lat, lon) {
+    fetchWeather: function (city) {
         fetch(
-            "https://api.openweathermap.org/data/2.5/weather?lat=" 
-            + lat 
-            + "&lon=" 
-            + lon 
+            "https://api.openweathermap.org/data/2.5/weather?q=" 
+            + city 
             + "&units=metric&appid=" 
-            + this.apiKey,
-        ).then((response)=> response.json())
-        .then((data)=> console.log(data));
+            + this.apiKey
+        )
+            .then((response)=> response.json())
+            .then((data)=> this.displayWeather(data));
     },
     displayWeather: function(data) {
         const {name} = data;
-        const {icon, description} = data.weather;
+        const {icon, description} = data.weather[0];
         const {temp, humidity} = data.main;
         const {speed} = data.wind;
+        console.log(name, icon, description, temp, humidity, speed);
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = temp + "°C";
+        document.querySelector(".humidity").innerText = "Humididty: " + humidity + "%";
+        document.querySelector(".wind").innerText = "Wind speed " + speed + " km/h";
     }
 };
  
